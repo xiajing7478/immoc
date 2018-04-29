13aaca62fd23ae3778c7d433871a0d3fc02a280f
xiajing7478 <260594736@qq.com>
2018-04-29 10:30:18 +0800

    add

--- src/views/geniusinfo/geniusinfo.js
+++ src/views/geniusinfo/geniusinfo.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react'
 class GeniusInfo extends Component{
 	render(){
 		return(
-			<div>GeniusInfo</div>
+			<div>
+				<h2>GeniusInfo</h2>
+				<ul>
+					<li>A</li>
+					<li>B</li>
+					<li>C</li>
+				</ul>
+			</div>
 		)
 	}
 }
