e8992ef82070c0d6a22ed17f7785de060c1a494c
Xia Jing <xiaj@gjmetal.com>
2018-05-10 18:28:25 +0800

    add less&scss

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import logoImg from './logo.jpg'
-import './logo.css'
+// import './logo.css'
+// import './logos.less'
+import './logos.scss'
 class Logo extends Component{
 	render() {
 		return (
 			<div className="logo-container">
-				<img alt="logo" src={logoImg} />
+				<img alt="logo" className="img-size" src={logoImg} />
 			</div>
 		)
 	}
